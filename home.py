606d1263d94e77d0bd5c624ba23d158934ccb064
CORNELIUS <126483961+Cornelius-BobCat@users.noreply.github.com>
2023-06-09 18:28:15 +0200

    Update home.py

--- home.py
+++ home.py
@@ -77,7 +77,7 @@ filtered_df = df[(df["annee"] == select_annee) & (df["type_local"] == "Apparteme
 pperm_a1 = filtered_df.groupby("mois").apply(lambda x: x["id_mutation"].count())
 combined_df_vol= pd.concat([pperm_m1, pperm_a1], axis=1)
 #combined_df_vol.columns = ['Maison', 'Appartement']
-combined_df_vol = combined_df_vol.rename(columns={"0": "Maison", "1": "Appartement"})
+combined_df_vol = combined_df_vol.rename(columns={0: "Maison", "1": "Appartement"})
 
 ############################################### MAINPAGE
 dyna_default = "Charente Maritime"
